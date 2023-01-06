f20c6741f0637b7f664fa93c9cc95f53ec66c850
dilaouid <dilaouid@student.42.fr>
2023-01-06 21:55:14 +0100

    add icon

--- gui/window.py
+++ gui/window.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 def configureWindow():
     window = tk.Tk()
-    window.title("Vaporwaver")
+    window.title("vaporwaver")
+    # define the window icon
+    window.iconbitmap("picts/icon.ico")
     window.geometry("1280x720")
     window.resizable(width=False, height=False)
     return window
