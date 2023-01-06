6cc7098d37ef179ba745161f90a4c97d2597ef07
dilaouid <dilaouid@student.42.fr>
2023-01-07 00:58:06 +0100

    update static gui

--- gui/sides/left.py
+++ gui/sides/left.py
@@ -3,20 +3,24 @@ from tkinter import ttk
 
 def leftFrame(window):
     left_frame = tk.Frame(window)
-    left_frame.configure(bg="#242424", bd=0, border=1, relief=tk.FLAT)
-    left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    left_frame.configure(bg="#242424", bd=0, border=1, relief=tk.FLAT, width=560, height=555)
+    left_frame.pack(side=tk.LEFT, fill=tk.BOTH, anchor=tk.NW, padx=5, pady=5)
 
     image = tk.PhotoImage(file="picts/background/default.png")
     preview_label = tk.Label(left_frame)
     preview_label.configure(image=image, border=1, relief=tk.RAISED)
     preview_label.image = image
-    preview_label.place(x=60, y=50, width=460, height=555)
+    preview_label.place(x=50, y=50, width=460, height=555)
     
     s = ttk.Style()
     s.configure("Dark.TButton", bg="red", fg="white", font=("Helvetica", 10), borderwidth=10, borderradius=20)
     s.configure("Save.TButton", bg="red", fg="white", font=("Helvetica", 10), borderwidth=10, borderradius=20)
     import_button = tk.Button(left_frame, text="Import")
-    import_button.place(x=60, y=555+60, width=300, height=30)
+    import_button.place(x=50, y=555+60, width=300, height=30)
 
     save_button = tk.Button(left_frame, text="Save", bg="blue", fg="white", state=tk.DISABLED)
-    save_button.place(x=370, y=555+60, width=150, height=30)
+    save_button.place(x=360, y=555+60, width=150, height=30)
+
+    # add a warning label
+    warning_label = tk.Label(left_frame, text="Warning: You need to import a character to start vaporwaving shits.", bg="#242424", fg="red", font=("Helvetica", 10))
+    warning_label.place(x=50, y=555+60+30, width=460, height=30)
--- gui/sides/right.py
+++ gui/sides/right.py
@@ -1,50 +1,111 @@
 import tkinter as tk
+from tkinter import ttk
 from gui.sides.frames.field import defineField
 
 def rightFrame(window):
     right_frame = tk.Frame(window)
     right_frame.configure(bg="#303030", bd=0, border=1, relief=tk.FLAT)
     right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+    right_frame.place(relx=0.53, rely=0.05)
 
-    posX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    posX.pack()
+    # Character edition (width, height, position, etc.)
+    posX_label = tk.Label(right_frame, text="Character X Position:", bg="#303030", fg="white")
+    posX_label.grid(row=0, column=0, padx=10)
+    posX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    posX.set(50)
+    posX.grid(row=1, column=0, padx=10, pady=10)
 
-    posY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    posY.pack()
+    posY_label = tk.Label(right_frame, text="Character Y Position", bg="#303030", fg="white")
+    posY_label.grid(row=0, column=1, padx=10)
+    posY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    posY.set(50)
+    posY.grid(row=1, column=1, padx=10, pady=10)
 
-    width = tk.Scale(right_frame, from_=0, to=100,orient=tk.HORIZONTAL)
-    width.pack()
+    width_label = tk.Label(right_frame, text="Character Width", bg="#303030", fg="white")
+    width_label.grid(row=0, column=2, padx=10)
+    width = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    width.set(50)
+    width.grid(row=1, column=2, padx=10, pady=10)
 
-    height = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    height.pack()
+    height_label = tk.Label(right_frame, text="Character Height", bg="#303030", fg="white")
+    height_label.grid(row=0, column=3, padx=10)
+    height = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    height.set(50)
+    height.grid(row=1, column=3, padx=10, pady=10)
 
-    selects = tk.Frame(right_frame)
-    selects.pack()
+    glitch_label = tk.Label(right_frame, text="Character Glitch (0-10)", bg="#303030", fg="white")
+    glitch_label.grid(row=2, column=0, padx=10)
+    glitch = tk.Scale(right_frame, from_=0, to=10, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    glitch.set(0)
+    glitch.grid(row=3, column=0, padx=10, pady=10)
 
-    bglabel = tk.Label(selects, text="Background")
-    bglabel.grid(row=0, column=0)
+    gradient_label = tk.Label(right_frame, text="Gradient", bg="#303030", fg="white")
+    gradient_label.grid(row=2, column=1)
+    gradient_var = tk.StringVar(right_frame)
+    gradient_var.set("None")
+    gradient = tk.OptionMenu(right_frame, gradient_var, "None", "Option 2", "Option 3")
+    gradient.grid(row=3, column=1)
 
-    bgvar = tk.StringVar(selects)
+    # Separator end of character edition
+    separator = tk.Frame(right_frame, bg='white', width=200, height=1)
+    separator.grid(row=4, column=0, columnspan=5, sticky=tk.EW, pady=10, padx=5)
+    ##############################
+
+    # Background and misc item management
+    bglabel = tk.Label(right_frame, text="Background", bg="#303030", fg="white")
+    bglabel.grid(row=5, column=0)
+    bgvar = tk.StringVar(right_frame)
     bgvar.set("Default")
-    bgel = tk.OptionMenu(selects, bgvar, "Default", "Option 2", "Option 3")
-    bgel.grid(row=1, column=0)
+    bg = tk.OptionMenu(right_frame, bgvar, "Default", "Option 2", "Option 3")
+    bg.grid(row=6, column=0)
 
-    fglabel = tk.Label(selects, text="Foreground")
-    fglabel.grid(row=0, column=1)
+    msclabel = tk.Label(right_frame, text="Misc Item", bg="#303030", fg="white")
+    msclabel.grid(row=5, column=1)
+    mscvar = tk.StringVar(right_frame)
+    mscvar.set("None")
+    msc = tk.OptionMenu(right_frame, mscvar, "None", "Option 1", "Option 2")
+    msc.grid(row=6, column=1, pady=10)
 
-    fgvar = tk.StringVar(selects)
-    fgvar.set("Nothing")
-    fgel = tk.OptionMenu(selects, fgvar, "Nothing", "Option 2", "Option 3")
-    fgel.grid(row=1, column=1)
+    msc_width_label = tk.Label(right_frame, text="Misc Width", bg="#303030", fg="white")
+    msc_width_label.grid(row=5, column=2, padx=10)
+    msc_width = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    msc_width.set(50)
+    msc_width.grid(row=6, column=2, padx=10, pady=10)
 
-    foregroundX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    foregroundX.pack()
+    msc_height_label = tk.Label(right_frame, text="Misc Height", bg="#303030", fg="white")
+    msc_height_label.grid(row=5, column=3, padx=10)
+    msc_height = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    msc_height.set(50)
+    msc_height.grid(row=6, column=3, padx=10, pady=10)
+    
+    msc_posX_label = tk.Label(right_frame, text="Misc X Position", bg="#303030", fg="white")
+    msc_posX_label.grid(row=7, column=2, padx=10)
+    msc_posX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    msc_posX.set(50)
+    msc_posX.grid(row=8, column=2, padx=10, pady=10)
 
-    foregroundY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    foregroundY.pack()
+    msc_posY_label = tk.Label(right_frame, text="Misc Y Position", bg="#303030", fg="white")
+    msc_posY_label.grid(row=7, column=3, padx=10)
+    msc_posY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL, bg="#303030", fg="white")
+    msc_posY.set(50)
+    msc_posY.grid(row=8, column=3, padx=10, pady=10)
 
-    backgroundX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    backgroundX.pack()
+    # Separator end of background and misc item edition
+    separator_second = tk.Frame(right_frame, bg='white', width=200, height=1)
+    separator_second.grid(row=9, column=0, columnspan=5, sticky=tk.EW, pady=10, padx=5)
+    ##############################
 
-    backgroundY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
-    backgroundY.pack()
+    # Visual options
+
+    crt_effect = tk.BooleanVar()
+    check_crt = tk.Checkbutton(right_frame, text="CRT Effect", variable=crt_effect, bg="#303030", fg="white")
+    check_crt.grid(row=10, column=0, padx=10, pady=10)
+
+    animate = tk.BooleanVar()
+    check_animate = tk.Checkbutton(right_frame, text="Animate", variable=animate, bg="#303030", fg="white")
+    check_animate.grid(row=10, column=1, padx=10, pady=10)
+
+    # disable all the widgets in the right frame except the separator and bgvar
+    for child in right_frame.winfo_children():
+        if child != separator and child != bg and child != separator_second:
+            child.configure(state=tk.DISABLED)
