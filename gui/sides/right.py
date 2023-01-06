4c661f9c5ead5ff081cf6ede282895f91ee4c4d8
dilaouid <dilaouid@student.42.fr>
2023-01-06 21:56:02 +0100

    remove useless and improve gui

--- gui/sides/right.py
+++ gui/sides/right.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from gui.sides.frames.field import defineField
-from gui.sides.frames.scale_bgfg import scales
 
 def rightFrame(window):
     right_frame = tk.Frame(window)
+    right_frame.configure(bg="#303030", bd=0, border=1, relief=tk.FLAT)
     right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
     posX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
@@ -21,6 +21,30 @@ def rightFrame(window):
     selects = tk.Frame(right_frame)
     selects.pack()
 
-    defineField("Foreground", selects)
-    defineField("Background", selects)
-    scales(right_frame)
+    bglabel = tk.Label(selects, text="Background")
+    bglabel.grid(row=0, column=0)
+
+    bgvar = tk.StringVar(selects)
+    bgvar.set("Default")
+    bgel = tk.OptionMenu(selects, bgvar, "Default", "Option 2", "Option 3")
+    bgel.grid(row=1, column=0)
+
+    fglabel = tk.Label(selects, text="Foreground")
+    fglabel.grid(row=0, column=1)
+
+    fgvar = tk.StringVar(selects)
+    fgvar.set("Nothing")
+    fgel = tk.OptionMenu(selects, fgvar, "Nothing", "Option 2", "Option 3")
+    fgel.grid(row=1, column=1)
+
+    foregroundX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
+    foregroundX.pack()
+
+    foregroundY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
+    foregroundY.pack()
+
+    backgroundX = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
+    backgroundX.pack()
+
+    backgroundY = tk.Scale(right_frame, from_=0, to=100, orient=tk.HORIZONTAL)
+    backgroundY.pack()
