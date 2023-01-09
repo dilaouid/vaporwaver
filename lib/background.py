393d3eb6a1f29d2d9f288e13cb46bcfc39c41d89
dilaouid <dilaouid@student.42.fr>
2023-01-09 23:29:41 +0100

    save the background in a global

--- lib/background.py
+++ lib/background.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from data import globals, gui
 
 def changeBackground(filename: str) -> None:
-    preview_bg = tk.PhotoImage(file='picts/backgrounds/' + filename + '.png')
+    globals["background"] = 'picts/backgrounds/' + filename + '.png'
+    preview_bg = tk.PhotoImage(file=globals["background"])
     gui["frame"]["canvas"].preview_bg = preview_bg
     gui["frame"]["canvas"].itemconfig(globals["background_container"], image=preview_bg)
