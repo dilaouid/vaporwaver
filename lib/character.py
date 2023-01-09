c612ee7f9990528359e26fd62f5c3b4de5167ce1
dilaouid <dilaouid@student.42.fr>
2023-01-09 02:28:11 +0100

    refactor

--- lib/character.py
+++ lib/character.py
@@ -4,6 +4,12 @@ from data import globals, gui
 from PIL import Image, ImageTk
 from glitch_this import ImageGlitcher
 
+def resizeAndUpdate():
+    image = Image.open(globals["characterPath"])
+    image = image.resize((int(image.size[0] * int(globals["val"]["characterScale"]) / 100), int(image.size[1] * int(globals["val"]["characterScale"]) / 100)), Image.LANCZOS)
+    image.save('./tmp/char.png')
+    return Image.open('./tmp/char.png')
+
 def moveCharacter(axis, value):
     if globals["character"] is None:
         return
@@ -15,30 +21,20 @@ def scaleCharacter(axis, value):
     if globals["character"] is None:
         return
     globals["val"]["characterScale"] = value
-    image = Image.open(globals["characterPath"])
-    image = image.resize((int(image.size[0] * int(globals["val"]["characterScale"]) / 100), int(image.size[1] * int(globals["val"]["characterScale"]) / 100)), Image.LANCZOS)
-    image.save('./tmp/char.png')
-    resized = Image.open('./tmp/char.png')
+    image = resizeAndUpdate()
+
     glitcher = ImageGlitcher()
-    glitched_image = glitcher.glitch_image(resized, color_offset=True, glitch_amount=float(globals["val"]["characterGlitch"]), seed=int(globals["val"]["characterGlitchSeed"]))
-    glitched_image.save('test.png')
+    glitched_image = glitcher.glitch_image(image, color_offset=True, glitch_amount=float(globals["val"]["characterGlitch"]), seed=int(globals["val"]["characterGlitchSeed"]))
     globals["gcChar"] = ImageTk.PhotoImage(glitched_image)
-    # create a new file with the image
-    
-
-
     gui["frame"]["canvas"].itemconfig(globals["character"], image=globals["gcChar"])
 
 def glitchCharacter(axis, value):
     if globals["character"] is None:
         return
     globals["val"][axis] = value
+    image = resizeAndUpdate()
 
-    image = Image.open(globals["characterPath"])
-    image = image.resize((int(image.size[0] * int(globals["val"]["characterScale"]) / 100), int(image.size[1] * int(globals["val"]["characterScale"]) / 100)), Image.LANCZOS)
-    image.save('./tmp/char.png')
-    resized = Image.open('./tmp/char.png')
     glitcher = ImageGlitcher()
-    glitched_image = glitcher.glitch_image(resized, color_offset=True, glitch_amount=float(globals["val"]["characterGlitch"]), seed=int(globals["val"]["characterGlitchSeed"]))
+    glitched_image = glitcher.glitch_image(image, color_offset=True, glitch_amount=float(globals["val"]["characterGlitch"]), seed=int(globals["val"]["characterGlitchSeed"]))
     globals["gcChar"] = ImageTk.PhotoImage(glitched_image)
     gui["frame"]["canvas"].itemconfig(globals["character"], image=globals["gcChar"])
